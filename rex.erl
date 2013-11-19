e4720698d693f5d4d26d158d19f0cd367f61bd33
Robbie Lynch <robbie.lynch@outlook.com>
2013-11-19 01:56:34 +0000

    eval update, correct minus

--- rex.erl
+++ rex.erl
@@ -1,11 +1,22 @@
 -module(rex).
--export ([eval/1]).
+-export ([parse/1,eval/1]).
 
+
+
+%Evaluation funtions
 eval({int, X}) -> X;
-eval({unOp, X, Y}) -> eval(Y);
-eval({binOp, add, L, R}) -> Answer = eval(L) + eval(R);
-eval({binOp, sub, L, R}) -> Answer = eval(L) - eval(R);
-eval({binOp, divide, L, R}) -> Answer = eval(L) / eval(R);
-eval({binOp, multiply, L, R}) -> Answer = eval(L) * eval(R).
+eval({unOp, X, Y}) -> -eval(Y);
+eval({binOp, add, L, R}) -> eval(L) + eval(R);
+eval({binOp, sub, L, R}) -> eval(L) - eval(R);
+eval({binOp, divide, L, R}) -> eval(L) / eval(R);
+eval({binOp, multiply, L, R}) -> eval(L) * eval(R).
 
 	
+%Take as input a String
+parse([]) -> [];
+parse([H|T]) when H =:= '~' ->
+	parse(T) ++ {unOp, minus}.
+	% parse(T),
+	% {bracket, left};
+	% parse(T),
+	% {bracket, right}.
