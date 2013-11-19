1521c5bf3fb6f79ff3c0ff11e241e9d244b8b61e
Robbie Lynch <robbie.lynch@outlook.com>
2013-11-19 16:45:22 +0000

    Create parse functions to extract expression into basic list of tuples.

--- rex.erl
+++ rex.erl
@@ -1,5 +1,5 @@
 -module(rex).
--export ([parse/1,eval/1]).
+-export ([parse/1,eval/1,p/1]).
 
 
 
@@ -11,12 +11,29 @@ eval({binOp, sub, L, R}) -> eval(L) - eval(R);
 eval({binOp, divide, L, R}) -> eval(L) / eval(R);
 eval({binOp, multiply, L, R}) -> eval(L) * eval(R).
 
-	
+p(S)->
+	lists:flatten([parse(S)]).
+
 %Take as input a String
 parse([]) -> [];
-parse([H|T]) when H =:= '~' ->
-	parse(T) ++ {unOp, minus}.
-	% parse(T),
-	% {bracket, left};
-	% parse(T),
-	% {bracket, right}.
+parse([H|T]) when H =:= 126 ->
+	lists:append([{unOp, minus}], parse(T));
+%Operands
+parse([H|T]) when H =:= 43 ->
+	lists:append([{binOp, add}], parse(T));
+parse([H|T]) when H =:= 45 ->
+	lists:append([{binOp, sub}], parse(T));
+parse([H|T]) when H =:= 42 ->
+	lists:append([{binOp, multiply}], parse(T));
+parse([H|T]) when H =:= 47 ->
+	lists:append([{binOp, divide}], parse(T));
+parse([H|T]) when H =:= 40 ->
+	lists:append([{bracket, left}], parse(T));
+parse([H|T]) when H =:= 41 ->
+	lists:append([{bracket, right}], parse(T));
+parse([H|T]) when (H > 47) and (H < 58) ->
+	lists:append([{int, H-48}], parse(T)).
+
+%numberBuilder([]) -> [];
+% numberBuilder([H|T])  when (H > 47) and (H < 58) -> [H] ++ numberBuilder(T);
+% numberBuilder(_) -> [].
